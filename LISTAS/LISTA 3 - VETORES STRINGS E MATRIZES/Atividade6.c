de34c5dc26962a85c4afb119359d547aa6413f03
Cássio Alcure Oliveira <cassioalcureoliveira@hotmail.com>
2023-02-10 13:01:34 -0300

    Troquei o nome da pasta onde as listas estão e adicionei uma pasta de exercícios

---
+++ LISTAS/LISTA 3 - VETORES STRINGS E MATRIZES/Atividade6.c
@@ -0,0 +1,49 @@
+/*Crie um programa em linguagem C que receba duas palavras de cinco letras. O programa deve
+calcular quantas letras idênticas na mesma posição existem entre as duas palavras. EX: P = "Corda"e
+Q = "carne", neste caso o programa deve retornar que existem 2 coincidências, sendo elas a letra
+’c’ na primeira posição e a letra ’r’ na terceira posição. Ao final, o programa deve imprimir quantas
+coincidências foram encontradas entre as duas palavras recebidas. Considere letras maiúsculas e
+minúsculas como iguais entre si, por exemplo ’a’ e ’A’ devem ser consideradas idênticas,
+assim como ’b’ e ’B’, etc.*/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define NUM 5
+
+int main()
+{
+
+    char palavra[NUM+1], palavra1[NUM+1];
+
+    while (1)
+    {
+        int i, j = 0;
+        \
+        printf("Digite uma palavra de 5 caracteres: ");
+        scanf("%s", palavra);
+        printf("Digite outra palavra de 5 caracteres: ");
+        scanf("%s", palavra1);
+
+        if (!(strlen(palavra) == NUM && strlen(palavra1) == NUM))
+        {
+            printf("As palavras tem que ter 5 caracteres.\n");
+            continue;
+        }
+
+        for (i = 0; i < NUM; i++)
+        {
+            palavra[i] = toupper(palavra[i]);
+            palavra1[i] = toupper(palavra1[i]);
+            if (palavra[i] == palavra1[i])
+            {
+                printf("A letra \"%c\" eh a mesma na posicao %d.\n", palavra[i], i + 1);
+                j++;
+            }
+        }
+        printf("A quantidade de letras iguais eh: %d.\n", j);
+        break;
+    }
+    return 0;
+}
