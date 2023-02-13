43c9c62880cfda11bd2473453e3175adc0b47ad0
Cássio Alcure Oliveira <cassioalcureoliveira@hotmail.com>
2023-02-13 02:37:33 -0300

    Questão 1 agora responde a todas as matrículas e eu adicionei todos os arquivos, cuz why not?

--- PROVA/Questao1.c
+++ PROVA/Questao1.c
@@ -4,25 +4,21 @@ linguagem C que decodifique a mensagem codificada contida no arquivo .dat cujo n
 seu número de matrícula. Tenha em mente que a mensagem codificada contida no arquivo
 .dat tem tamanho de 200 números inteiros. Após decodificada, a mensagem exibirá um número
 inteiro de 7 dígitos (valor entre 1.000.000 e 9.999.999) que deve ser preenchido na
-prova.
-O código criado para solucionar esta questão deve ser anexado à prova.
+prova. O código criado para solucionar esta questão deve ser anexado à prova.
 Número de 7 dígitos Após decodificado, o arquivo conterá uma mensagem e um código
 numérico de 7 dígitos que deve ser anexada à prova.
 
 Decodificação O método a ser aplicado utiliza uma operação matemática simples aplicada
-
 entre cada letra da mensagem codificada C e da chave K, progressivamente. A operação mate-
 mática que será utilizada na codificação chama-se XOR, matematicamente é simbolizada por ⊕
-
 e na linguagem C é executado utilizando o símbolo de acento circunflexo (^).
 Quando estamos de posse da mensagem codificada C e da chave K e temos o interesse de gerar
 a mensagem M, este processo é chamado de decodificação. Este processo é feito aplicando a
 mesma operação XOR, uma a uma, entre cada número da mensagem codificada C e cada letra
 da chave K, gerando assim cada letra da mensagem M.
 
-Exemplo de decodificação Supondo que o valor da mensagem codificada C e da chave K
-sejam C = {18, 17, 14, 4, 12, 0,
-
+Exemplo de decodificação Supondo que o valor da mensagem codificada C e da chave K sejam
+ C = {18, 17, 14, 4, 12, 0,
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,11 +27,18 @@ sejam C = {18, 17, 14, 4, 12, 0,
 int main()
 {
     int codificada[200];
-    char chave[] = "2022201305";
+    char chave[20];
     char mensagem[200];
-    int tamanho_mensagem = 200, tamanho_chave, opcao, i;
+    int tamanho_mensagem = 200, tamanho_chave, i;
+    char arquivo_nome[30];
 
-    FILE *arquivo = fopen("2022201305.dat", "r");
+    printf("Digite o numero de matricula: ");
+    scanf("%s", chave);
+
+    // criar o nome do arquivo a partir da chave
+    sprintf(arquivo_nome, "%s.dat", chave);
+
+    FILE *arquivo = fopen(arquivo_nome, "r");
 
     if (arquivo == NULL)
     {
